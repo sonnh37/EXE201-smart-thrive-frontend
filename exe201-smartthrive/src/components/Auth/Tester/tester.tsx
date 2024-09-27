3313740fd00f9547dd8a75f47b1b83cb34ebf852
Cao Quan <quancao08@gmail.com>
2024-09-27 08:36:31 +0700

    fix mot so loi

--- exe201-smartthrive/src/components/Auth/Tester/tester.tsx
+++ exe201-smartthrive/src/components/Auth/Tester/tester.tsx
@@ -1,16 +1,37 @@
-"use client";
-import React from "react";
-import { Button } from "@/components/ui/moving-border";
+// components/DrawOutlineButton.tsx
 
-export function MovingBorderDemo() {
-  return (
-    <div>
-      <Button
-        borderRadius="1.75rem"
-        className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
-      >
-        Borders are cool
-      </Button>
-    </div>
-  );
+import React , {useState} from "react";
+
+interface DrawOutlineButton  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode; // Type for children
 }
+
+const DrawOutlineButton: React.FC<DrawOutlineButton > = ({ children, ...rest }) => {
+
+
+  return (
+    <button
+      {...rest}
+      className="group relative px-4 py-2 font-medium text-slate-100 transition-colors duration-[400ms] hover:text-indigo-300"
+
+    >
+      <span>{children}</span>
+
+      {/* TOP */}
+      <span className="absolute left-0 top-0 h-[2px] w-0 bg-indigo-300 transition-all duration-100 group-hover:w-full" />
+
+      {/* RIGHT */}
+      <span className="absolute right-0 top-0 h-0 w-[2px] bg-indigo-300 transition-all delay-100 duration-100 group-hover:h-full" />
+
+      {/* BOTTOM */}
+      <span className="absolute bottom-0 right-0 h-[2px] w-0 bg-indigo-300 transition-all delay-200 duration-100 group-hover:w-full" />
+
+      {/* LEFT */}
+      <span className="absolute bottom-0 left-0 h-0 w-[2px] bg-indigo-300 transition-all delay-300 duration-100 group-hover:h-full" />
+    </button>
+    
+  );
+
+};
+
+export default DrawOutlineButton ;
